ef42de2d5f1ceffb6cd60b3dc5d1c8133d81fa2c
robertsamboy <106219442+robertsamboy@users.noreply.github.com>
2022-07-16 16:52:17 -0400

    Create review.js

---
+++ review.js
@@ -0,0 +1,530 @@
+/* 
+                     __ 
+                  | /   
+                  |(___ 
+                  )    )
+                _/  __/ 
+                        
+        __                       
+       /  |           /          
+      (___| ___         ___      
+      |\   |___) \  )| |___)|   )
+      | \  |__    \/ | |__  |/\/ 
+
+*/
+
+// Instructions: Follow the prompts and make sure to check your answer in the console. Some of the methods I am requiring for you to us we didn't do in class yet. That's on purpose, if you need assistance look back in lessons, and of course Google. :) 
+
+// =================================================
+//                     VARIABLES
+// =================================================
+
+// 1.) Declare a variable
+var x = 1;
+
+
+// 2.) Change the value of this variable
+console.log(x);
+
+
+var x = 5
+console.log(x)
+
+// 3.) Change the data type of this variable
+
+var x = 'hey'
+console.log(x)
+// 4.) Create another variable and the one from above to concatenate
+
+let y = 'hello'
+console.log(y+x)
+// 5.) Use any of the variables above or create new ones and print using string interpolation
+var str = `${x} how are you`
+
+console.log (str)
+// ================ Variable Names =================
+/* 
+    • Case-sentive! Try it out! :O
+    • Can contain letters, digits, or the symbols $ and _
+    • Can't start with 0-9
+    • Can't use reserved keywords in JS such as var, function, return, etc. 
+*/
+
+// =================================================
+//                      STRINGS
+// =================================================
+
+// Look up the following string methods!
+
+// Using charAt(), print the letter i in Sabrina
+var x = 'Sabrina'
+console.log(x.charAt(4))
+// Printed i, typed code in console.log
+
+
+// Define Unicode in a comment, and get the Unicode using charCodeAt() of the third character in Teddy
+// Unicode is a global encoding standard for use from languages and code, Unicode translates the code within the device/internet for our understanding. 
+var z = 'Teddy'
+console.log(z.charCodeAt(2)) 
+
+
+
+// Using fromCharCode() - make it readable for us :). You'll see!
+console.log(String.fromCharCode(100))
+// Take your first and last name and concat()
+var r = 'Robert '
+var s = 'Samboy'
+console.log(r.concat(s))
+
+// Create a string and make it return true using startsWith()
+var f = 'heyo'
+console.log(f.startsWith('heyo'))
+// Now use any variable with endsWith() and return false
+console.log(r.endsWith())
+
+// Finish the following sentence. Use includes() and return true.
+const ozgur = 'Once upon a time Ozgur looked up at the Moon, '
+console.log(ozgur.includes('Once upon a time Ozgur looked up at the Moon, '))
+
+// Help! I'm looking for my cow! Using indexOf() help Josh find his cow. What's the index of cow?
+const joshHadALittleLambOopsCow = "Josh lives in a farm. Josh was overwhelmed with coding. Josh is stressed out. Josh fed the chickens this morning. He watered his plants. He took his cow Betsy on a walk. Josh went home without Betsy. Josh went to bed that night, and realized he went home alone."
+console.log(joshHadALittleLambOopsCow.indexOf('cow'))
+// Oh no! The alien doppelgangers are here. Let's use our laserbeam lastIndexOf() to shoot down the last alien named Vanessa in the String Universe.
+
+const vanessa = "I am the Alpha Vanessa. I am the Omega Vanessa. We do not come in peace."
+console.log(vanessa.lastIndexOf('Vanessa'))
+// Can we use length for strings? I don't know, you tell me.
+const noWeCantTeo = "but did you try it out though?"
+console.log(noWeCantTeo.length + ' yes we can')
+// If only it was this easy to replace() my ex. Totally not joking, LOL. JK
+const replaceGokuWithVegeta = "For my DBZ fans, we all know Goku is the best!"
+console.log(replaceGokuWithVegeta.replace('Goku', 'Vegeta'))
+
+// Josh has been forgetful lately. Let's help Josh by using search(), and find their right 'wifey'. 
+const joshIsLookingForWifey = "WIFEY 1, wifey 2, WiFeY 3, Wifey 4"
+console.log(joshIsLookingForWifey.search('Wifey'))
+
+// I guess we can share this pizza sentence. Use slice() to return the other half of pizza.
+const pizzaSentence = "pizza, other half of pizza"
+console.log(pizzaSentence.slice(7))
+// Now using the pizza sentence, return only pizza (before the comma)
+console.log(pizzaSentence.slice( 0,-20))
+// Okay, but who decided to go to this expensive restaurant? Yaz, Poornima, and Leshawn decided to split() the bill. Return an array separating Yaz, Poornima, and Leshawn.
+const splitTheBill = "Yaz, Poornima, and Leshawn"
+console.log(splitTheBill.split(","))
+// OH NO! The alien invaders from earlier were so against bill splitting so they decided to split() Yaz, Poornima, and Leshawn into pieces. Using splitTheBill, return an array separating all three by characters.
+console.log(splitTheBill.split(""))
+// Use this toLowerCase()
+const angry = "LOWER YOUR VOICE DOWN IF YOU AIN'T PAYING MY BILLS."
+// Tuan was angry today. Create a string with words that Tuan would yell out. Now lowercase it, to tell Tuan to chill out. 
+console.log(angry.toLowerCase())
+// toUpperCase()
+const jahlunSaidToPutSomeRespectToHisName = "jahlun"
+console.log(jahlunSaidToPutSomeRespectToHisName.toUpperCase())
+// substring()
+// on the chopping block...
+
+// Returns "ell"
+const basicGreeting = "Hello World"
+console.log(basicGreeting.substring(1,4))
+// Returns "JavaScript"
+const ohReally = "JavaScript Substring"
+console.log(ohReally.substring(0,10))
+// Returns aol.com
+const aslDays = "xXteoWuzHereXx@aol.com"
+console.log(aslDays.substring(15,22))
+// trim()
+// If only I can use this for my love handles...
+// Create a variable with a string value with some extra loving on both sides.
+ const lovehandletax = '          I gotta bring tissues everwhere with me          '
+ console.log(lovehandletax.trim())
+// =================================================
+//                     BOOLEAN
+// =================================================
+
+// I just want my dreams to come true. 
+// With the powers of logical and comparison operators, PRINT TRUE please!
+// Replace the underscores.
+
+const a = 5;
+const b = 10;
+const c = 15;
+const d = "Keke"
+
+ console.log(a < b)
+ console.log(c > b)
+ console.log(d <= d)
+ console.log(d != a)
+console.log(a < 15)
+console.log(a < b < c)
+console.log(c > b > a != d)
+
+// =================================================
+//                     LOOPS
+// =================================================
+
+// Ken has a headache today and he said the room has been spinning like crazy. Make Ken's room spin 10 times using FOR LOOP. Make him print a sentence.
+for(room = 0; room <=10; room++ ){
+  console.log(room + ' Am I dizzy?')
+
+}
+console.log('i think i am')
+// Harry said that was so mean of Teo to do Ken dirty like that. Make Teo's vision spin 20 times FOR LOOP.
+for(vision = 1; vision <=20; vision++){
+  console.log(vision)
+}
+
+
+// But wait! Lisa said, while Teo is spinning 20 times. Let's make him say "I'm sorry". Print I'm sorry 20 times using a WHILE loop.
+var spin = 1
+while(spin<=19){
+  
+  spin++
+  console.log(spin + "I'm sorry")
+  
+}
+
+// For the next three, look for the syntax of FOR IN and FOR OF loop. Very easy to understand syntax, and very clean :) 
+
+
+// To The Left! To The Left! Everything I want in the console to the left. Print thing1, thing2, thing 3 using (FOR IN LOOP)
+const whateverQueenBeySaid = {
+  thing1: 'shoes',
+  thing2: 'clotes',
+  thing3: 'gaming console'
+}
+for(const why in whateverQueenBeySaid){
+  console.log(`${why}`)
+}
+// Using (FOR IN LOOP), print the indexes of the array.
+const lana = ['l', 'a', 'n', 'a']
+for(var uhm in lana){
+  console.log(`${lana[uhm]}`)
+}
+// USE (FOR OF LOOP)!
+const tia = ['teo', 'the', 'syntax', 'of', 'this', 'is', 'cleaner']
+for(const hm of tia){
+  console.log(hm)
+  // so rather using the dollar sign symbol, I can just type out the variable or string?? whats the use of the for in loop?
+  
+}
+
+// Look into forEach() loop, it accepts a callback aka a function inside a function. 
+// Swathi said don't forget each person. No person left behind! Use forEach() to iterate over the array perscholas. Inside the function, create a variable with the string "PS". Print their name in the console concatenated with the variable. 
+const perscholas = ['Amira', 'Arely', 'Jonathan']
+var g = 'PS'
+perscholas.forEach(element => console.log(element + ` ${g}`));
+
+// =================================================
+//                     ARRAYS
+// =================================================
+
+// Look up the following: Make a note of what each one of these methods do
+// push()
+// pop()
+// unshift()
+// shift()
+// concat()
+// splice()
+// slice()
+// sort()
+// includes()
+// indexOf()
+// length
+
+const fruits = ['apple', 'banana', 'orange']
+
+// Print banana
+console.log(fruits[1])
+// Let's turn it up a notch. I won't tell you what array methods to use. 
+// but why?
+
+// Join all the elements of the array into a string separated by a space.
+console.log(fruits.join(" "));
+// Remove orange
+ fruits.pop();
+console.log(fruits);
+
+// Add strawberry, kiwi, and grapes at the end
+fruits.push('strawberry', 'kiwi', 'grapes')
+console.log(fruits)
+// Remove apple
+
+console.log(fruits.shift())
+// Add mango at the beginning of the array
+fruits.unshift('mango')
+console.log(fruits)
+// Add lemon, and grapefruit between mango and banana
+fruits.splice(1,0, 'lemon')
+fruits.splice(2,0, 'grapefruit' )
+console.log(fruits)
+
+
+// Remove banana and strawberry
+fruits.splice(3,2)
+console.log(fruits)
+
+
+// Create a new array called exoticFruits. Add 3 exotic fruits inside of this array. Create a new variable and assign it with the value of concatinating fruits array and exoticFruits array.
+const exoticFruits = ['Avocado','Salak','Chempedak ']
+const fruitbasket = fruits.concat(exoticFruits);
+// Print the last two exotic fruits without altering the newly concatenated array.
+console.log(fruitbasket[6],fruitbasket[7])
+// Monalissa said she needs help re-organizing her items in alphabetical order.
+const monalissaIsMessy = ["pencil", "paper", "notebook", "computer"]
+console.log(monalissaIsMessy.sort())
+// Kevin accidentally stepped into the mirror world and needs help reversing his words.
+const mirrorMirrorOnTheWall = ["all", "them", "of", "greatest", "the", "is", "who"]
+console.log(mirrorMirrorOnTheWall.reverse())
+// ===== HIGHER ORDER METHODS =====
+// Rafael said he''s trying to stop cursing so much, and he wants to start with his text messages. Help him write code to eliminate the badword in his sentences. Must return a string sentence. Must use filter(). :) 
+const RafaelNoBadWords = "I am so tired of this badword food. I do not want this badword badword food."
+let changetoarray = RafaelNoBadWords.split(" ")
+let result = changetoarray.filter(n => n.length < 7)
+let returntostring = result.join(' ')
+console.log(returntostring)
+// Man math is hard. Let's make our computer do it for us. Use reduce() to find the total of the numbers in numbersToAddUp
+const numbersToAddUp = [99, 5677, 232, 84, 2, 231]
+
+var sumup = numbersToAddUp.reduce((l,n) => l + n)
+console.log(sumup)
+// ===== SPREAD OPERATOR ===== 
+
+// Create a variable and assign a value of a copied array using the spread operator
+var ok = [1,2,3, ...exoticFruits]
+
+console.log(ok)
+
+// Create two arrays with any elements and connect both of them using the spread operator. Save the connected arrays into a new variable.
+var hel = ["I'm"]
+var lo = ["ok"]
+let hello = [...hel, ...lo]
+console.log(hello)
+// Using the variable with the newly connected arrays, use spread operator to add something at the end.
+var ok = [...hello, ...lovehandletax]
+console.log(ok)
+
+
+// Using the variable with the newly connected arrays, use spread operator to add something at the beginning.
+var ko = [...fruits, ...hello]
+console.log(ko)
+// ===== ACCESS =====
+
+// Donut Shopping
+const donutShop =[
+  [
+    'firecracker',
+    'chocolate',
+    'blueberry'
+  ],
+  [
+    ['orange', 'applecrumble'],
+    ['cream', 'peanutbutter', 'coconut']
+  ],
+  [
+    [
+      [
+        [
+          ['flavor of the week']
+        ], ['ihatethis']
+      ]
+    ]
+  ]
+]
+
+// Anthony wants applecrumble. Print please!
+// Tosi wants ihatethis. :) Print!
+var why = donutShop[1]
+var please = why[0]
+var neveragain = please[1]
+console.log(neveragain)
+var friend = donutShop [2]
+var friendly = friend[0]
+var friendlier = friendly[0]
+var friendliest = friendlier[1]
+console.log(friendliest[0])
+// =================================================
+//                     IF/ELSE
+// =================================================
+
+// Teo is a person. I would hope so...
+// Check if Teo is a person. If true, print 'Teo is a person!' if not then print 'Teo is not a person!'
+var m = 'Teo is a person'
+if(m === 'Teo is a person'){
+  console.log('Teo is a person!')
+}else {
+  console.log('Teo is not a person')
+}
+// If Teo is a person, check to see if he is a male or a female. If male, print 'You got it right!' if not then print 'Wrong. Teo is going to remove you from the class.'.
+var f = 'female'
+if(f === 'male'){
+  console.log('You got it right!')
+}else{
+  console.log('Teo is going to remove you from the class.')
+}
+
+// If male, check to see if Teo is hungry. If Teo is 'hungry' then print 'Let's buy Teo some tacos!'. If Teo is 'not hungry' then print 'He doesn't need to eat.'. Anything else, print 'If Teo is not hungry, am I hungry?'
+var stomach = 'not hungry'
+if(stomach === 'hungry'){
+  console.log("Let's buy Teo some tacos!")
+}else if(stomach === 'not hungry'){
+console.log('He doesnt need to eat.')
+}else{
+  console.log('If Teo is not hungry, am I hungry?')
+ 
+}
+
+// Create a if/else ternary to check if Gustavo is the coolest. If coolest, print 'You got that right!', else print 'James wants to argue. He says he's the best!'
+
+var Gustavoisthecoolest = 'Gustavo is the coolest'
+var cool = Gustavoisthecoolest ? "You got that right!":"James wants to argue. He says he's the best!";
+console.log(cool)
+// not too sure about this operator, whats the point of it? and how do I get it to print the other string?
+
+// =================================================
+//                     FUNCTIONS
+// =================================================
+
+// Don't forget to call your functions :)
+
+// Create a function called sayGreeting and PRINT 'Hello, ma'amsir!'
+function sayGreeting (){
+  return "Hello, ma'amsir!";
+
+}
+console.log(sayGreeting())
+// Create a function that takes a string as a parameter. PRINT 'This is my (STRING)'. Please replace the (STRING) with the parameter. Don't play with me haha.
+function string(z){
+
+return z = 'family'
+}
+console.log(`This is my ${string()}`)
+// Create a function called add that takes in three number parameters. Create 3 variables and assign numerical values to them. Invoke your add() and pass in those three variables as arguments.
+function add(x,y,z){
+
+  return x + y + z
+}
+console.log(add(1,2,3))
+// Create a function called fightClub() that accepts a name parameter.  If the name:
+function fightClub(){
+  n = 'Devin'
+  if(n === 'Teo'){
+return '1st rule: You do not talk about Fight Club.'
+  }else if( n === 'Manara'){
+    return '2nd rule: YOU DO NOT TALK ABOUT FIGHT CLUB.'
+  }else if(n === 'Liv'){
+    return '3rd rule: If someone yells "Stop!", goes limp, taps out, the fight is over.'
+  }else if(n === 'Devin'){
+    return '4th rule: Only two guys to a fight.'
+  }else{
+    return 'No shirts'
+  }
+}
+console.log(fightClub())
+// 'Teo' RETURN '1st rule: You do not talk about Fight Club.'
+// 'Manara' RETURN '2nd rule: YOU DO NOT TALK ABOUT FIGHT CLUB.'
+// 'Liv' RETURN '3rd rule: If someone yells "Stop!", goes limp, taps out, the fight is over.'
+// 'Devin' RETURN '4th rule: Only two guys to a fight.'
+// Anything else, RETURN 'No shirts
+
+// Create a function called hello() that prints 'Hello'. Return an anonymous function inside hello() and have it print 'Goodbye'. Invoke your hello function in some way to see Hello and Goodbye in the console.
+function helloe(z = 'hello'){
+
+  var cya = function goodbye(){
+    return z = 'hello'
+  }
+  return cya = 'goodbye ' + z
+
+}
+console.log(helloe())
+
+// couldnt use 'hello' because the terminal is too nitpicky 
+// Create a function expression with your first name as the variable and then print your first and last name
+const Robert = function(){
+  return 'Robert Samboy'
+}
+console.log(Robert())
+// Create an arrow function that accepts a number and have it return that number doubled
+ let that = g => g * 2
+
+console.log(that(2))
+// =================================================
+//                     OBJECTS
+// =================================================
+
+// Create an object and call it human. Add a name, age, and location property. Give the properties values.
+const human = {
+  name: 'Johnny',
+  age: 24,
+  locationproperty: 'THE Bronx' 
+}
+
+// Access the name using dot notation
+console.log(human.name)
+
+// Access the age using square brackets
+console.log(human['age'])
+// Use object destructuring to access location
+let locationproperty = human.locationproperty
+console.log(locationproperty)
+// ACCESS Granted
+const bulbasaur = {
+  name: 'Bulbasaur',
+  abilities: [
+    {
+      ability: 'overgrow'
+    },
+    {
+      ability: 'chlorophyll'
+    }
+  ],
+  moves: ['razor-wind', 'swords-dance', 'cut'],
+  sound: function () {
+    console.log('Bulbahhhh!!!!!')
+  }
+}
+
+// Print overgrow
+var again = bulbasaur.abilities
+var again1 = again
+var again2 = again1[0]
+var again3 = again2
+console.log(again3.ability)
+
+// Print cut
+var repeat = bulbasaur.moves
+var repeat1 = repeat[2]
+console.log(repeat1)
+
+// Print Bulbahhhh!!!!!
+
+var insanity = bulbasaur.sound
+var insanity1 = insanity()
+console.log(insanity1)
+// Add a height of 7 to bulbasaur using the dot notation. (Don't change bulbasaur object manually)
+bulbasaur.height = 7;
+console.log(bulbasaur)
+// Add a property called order and assign it a value of 1 using the square brackets. (Don't change bulbasaur object manually)
+bulbasaur['order'] = 1;
+console.log(bulbasaur)
+// Print an array that contains every single properties in bulbasaur
+
+var array = Object.keys(bulbasaur)
+    .map(function(key) {
+        return bulbasaur[key];
+    });
+    console.log(bulbasaur)
+
+// Print every single properties one by one in the console
+var prop = Object.keys(bulbasaur).join()
+console.log(prop)
+// Print an array that contains every single values in bulbasaur
+var array1 = Object.keys(bulbasaur).map(function(key) {
+return bulbasaur[key]
+})
+console.log(array1.join())
+
+// var array1 = array[0]
+// var array2 = array[1]
+// var array3 = array2[0]
+// console.log(array3)
